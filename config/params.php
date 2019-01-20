02738eb90d79b25a834f0a799c913d202ef2ae73
BlackSpel <43000402+BlackSpel@users.noreply.github.com>
2019-01-20 15:00:09 +0500

    Edit mail

--- config/params.php
+++ config/params.php
@@ -2,12 +2,12 @@
 
 return
 [
-  'admin_email' => 'mr.timur123@mail.ru',
+  'admin_email' => '',
   'shop_name' => ' Buy Watches online For Men and Women | ALLStuff.ru',
   'pagination' => 3,
   'smtp_host' => 'smtp.gmail.com',
   'smtp_port' => '465',
   'smtp_protocol' => 'ssl',
-  'smtp_login' => 'allstuff.noreply@gmail.com',
-  'smtp_password' => 'Timur123',
+  'smtp_login' => '',
+  'smtp_password' => '',
 ];
